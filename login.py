41ea83d2ec8b3390e40137e3fdd5a1c861d623b7
Maddie Ostergaard <Mostergaard@stuy.edu>
2016-09-29 23:51:15 -0400

    finishing up

--- login.py
+++ login.py
@@ -3,25 +3,32 @@ from flask import Flask, render_template, request # Capital Flask is a subset of
 app = Flask(__name__)
 
 
-
 # Home page~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 @app.route("/") # decorator, goes directly above funtion header, the 
                 # "/" (route) for webpage will run this function
 @app.route("/login/")
 
+
 def welcome():
     print request.headers # contains metadata about request
+    return render_template('login.html')
 
 
-    return render_template('button.html')
 
+# Authentication page~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 @app.route("/authenticate/", methods = ["GET", "POST"]) # routed to after submit bc of 
                              # form action = "/authenticate/"
-
-def auth():                # NOTE: we are using get requests
-	print request.form
-	print request.form['user']
-	return "OK"
+def auth():                # NOTE: we are using POST requests
+	
+	user = "cleo"
+	pswd = "patra"
+	filled_form = request.form
+	if filled_form["username"] == user and filled_form["password"] == pswd:
+		success = "You have successfully logged in!"
+	else :
+		success = "You have failed to log in :("
+	
+	return render_template('authenticate.html', header = success )
 
 if __name__ == '__main__':
     app.debug = True
