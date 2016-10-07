0d76496de0ac91a37b4a048340ec0497901ae88d
Maddie Ostergaard <Mostergaard@stuy.edu>
2016-10-07 14:31:16 -0400

    started adding see=ssion stuff

--- login.py
+++ login.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request # Capital Flask is a subset of the module flask
+from flask import Flask, render_template, request, session, url_for# Capital Flask is a subset of the module flask
 import hashlib
 
 app = Flask(__name__)
+app.secret_key = '%t\x15\x94\x00\x11g\x98\x13\xcexI].\xc3F\xdaM\xfeB\x9ay\x11\xc7\x81lG:\xc8\xd8y\x8a'
 
 
 # Home page~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -17,8 +18,8 @@ def welcome():
 @app.route("/register/") 
 
 def login():
-	with open("passwords.txt" , "a") as f:
-		f.write("Testing 123\n")          
+	with open("passwords.txt" , "a") as PassWordFile:
+		PassWordFile.write("Testing 123\n")          
 	return render_template('login.html', footer = "You have created a new account!")
    
 
@@ -32,7 +33,7 @@ def login():
 	success = "You have created a new account!"
 	return render_template('login.html', footer = success)
     
-	open("passwords.txt", "a") as PassWordFile
+	with open("passwords.txt", "a") as PassWordFile
     for line in PasswordFile.readlines():
     	account = line.split()
     	if account[0] == filled_form["username"]:
